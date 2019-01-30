8dd558c930d2202a0afb0b14d596f26f5f58f373
EPAM RD NET Lab, Minsk <31510338+EPM-RD-NETLAB@users.noreply.github.com>
2019-01-30 16:12:36 +0300

    Add project files.

---
+++ WordsManipulation.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System;
+
+namespace PadawansTask8
+{
+    public static class WordsManipulation
+    {
+        public static void RemoveDuplicateWords(ref string text)
+        {
+            // put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
